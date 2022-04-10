9b24c92c0cb96daa4f2541bce0662c8497e32fff
ferrari <contaferrari2005@gmail.com>
2022-04-10 00:22:35 -0300

    A Diamite

--- src/ferrari/github/Main.java
+++ src/ferrari/github/Main.java
@@ -2,7 +2,6 @@ package ferrari.github;
 
 import ferrari.github.commands.GetDiamond;
 import ferrari.github.events.DropItemEven;
-import ferrari.github.events.ExplosionEvent;
 import ferrari.github.events.RightClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
@@ -13,7 +12,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("pegardiamante").setExecutor(new GetDiamond());
-        getServer().getPluginManager().registerEvents(new ExplosionEvent(), this);
         getServer().getPluginManager().registerEvents(new DropItemEven(), this);
         getServer().getPluginManager().registerEvents(new RightClickEvent(), this);
         Bukkit.getConsoleSender().sendMessage("§aFerrari has been Plugin Enabled!");
